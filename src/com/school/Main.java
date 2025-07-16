abac1afafa2b85c25fe927c91639b83f345fc6e5
Mannat Garg <mannatgarg2006@gmail.com>
2025-07-16 09:44:15 +0530

    Session 2

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,7 +2,41 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the School Attendance System Project!");
-        System.out.println("Session 1: Project Setup and Orientation Complete.");
+
+        System.out.println("---School Attendance System---");
+
+        Student[] students = new Student[2];
+
+        Student S1 = new Student();
+        Student S2 = new Student();
+
+        S1.setDetails(01, "Mannat");
+        S2.setDetails(02, "Harsh");
+
+        students[0] = S1;
+        students[1] = S2;
+
+        System.out.println("\nRegistered Students:");
+
+        for (int i = 0; i < students.length; i++) {
+            students[i].displayDetails();
+        }
+
+        Course[] courses = new Course[2];
+
+        Course C1 = new Course();
+        Course C2 = new Course();
+
+        C1.setDetails(01, "OOP");
+        C2.setDetails(02, "DBMS");
+
+        courses[0] = C1;
+        courses[1] = C2;
+
+        System.out.println("\nAvailable Courses:");
+
+        for (int i = 0; i < courses.length; i++) {
+            courses[i].displayDetails();
+        }
     }
 }
